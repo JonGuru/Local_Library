a40eb082706afad48dbdfbfb87553ac5826d87cc
Jonatan Gonzalez <76608291+JonGuru@users.noreply.github.com>
2023-09-05 10:09:34 -0500

    home.js
    Ive realized that in the code uploaded that the helper function was missing on line 20.

--- public/src/home.js
+++ public/src/home.js
@@ -17,6 +17,8 @@ function getBooksBorrowedCount(books) {
   // Hint: You can use the [`filter()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter) method here. 
   // If you get stuck, feel free to take a look at this repl.it: https://replit.com/@thinkful/getBooksBorrowedCount#index.js
 
+  function getBooksBorrowedCount(books) // This is the helper function I used to support this task.
+  {
     const listBooks = books.filter((book) => !book.borrows[0].returned);
     let count = 0;
     function bookCount(listBooks)
